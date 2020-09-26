0fcac083d055b1981a37edb5a298f0d8edb8a3cf
bonggal.siahaan <bonggal.siahaan@dana.id>
2020-09-26 14:14:57 +0700

    show username in home page

--- src/main/java/com/prometheus/ledger/controller/main/MainController.java
+++ src/main/java/com/prometheus/ledger/controller/main/MainController.java
@@ -2,6 +2,9 @@ package com.prometheus.ledger.controller.main;
 
 import com.prometheus.ledger.service.common.session.SessionService;
 import com.prometheus.ledger.service.common.session.result.GetLoginSessionResult;
+import com.prometheus.ledger.service.facade.member.MemberFacade;
+import com.prometheus.ledger.service.facade.member.request.QueryMemberRequest;
+import com.prometheus.ledger.service.facade.member.result.QueryMemberResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
@@ -18,10 +21,18 @@ public class MainController implements ErrorController {
     @Autowired
     private SessionService sessionService;
 
+    @Autowired
+    private MemberFacade memberFacade;
+
     @RequestMapping(value = {"/","/index"}, method = RequestMethod.GET)
     public String mainPage(HttpServletRequest request, HttpServletResponse response, Model model){
         GetLoginSessionResult result = sessionService.getLoginSession(request.getSession());
         model.addAttribute("userId", result.getUserId());
+
+        QueryMemberRequest queryMemberRequest = new QueryMemberRequest();
+        queryMemberRequest.setUserId(result.getUserId());
+        QueryMemberResult queryMemberResult = memberFacade.queryMember(queryMemberRequest);
+        model.addAttribute("username", queryMemberResult.getMember().getUsername());
         return "index";
     }
 
